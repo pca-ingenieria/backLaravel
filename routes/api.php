c2c64e734c67c2e7a7c7826b25405aa3c11a54c7
Roosevelt Diaz <roosevelt.diaz@iteria.com.co>
2019-07-15 15:58:59 -0500

    commit final

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/getusers', 'ApiController@getUsers')->middleware('cors');
-Route::get('/getusersautomatic', 'ApiController@getUsersAutomatic')->middleware('cors');
-Route::post('/saveuser', 'ApiController@saveUser')->middleware('cors');
-Route::post('/updatebet', 'ApiController@updateBet')->middleware('cors');
-Route::get('/showuser/{id}', 'ApiController@showUser')->middleware('cors');
-Route::post('/edituser', 'ApiController@editUser')->middleware('cors');
-Route::get('/deleteuser/{id}', 'ApiController@deleteUser')->middleware('cors');
+Route::get('/getusers', 'Apicontroller@getUsers')->middleware('cors');
+Route::get('/getusersautomatic', 'Apicontroller@getUsersAutomatic')->middleware('cors');
+Route::post('/saveuser', 'Apicontroller@saveUser')->middleware('cors');
+Route::post('/updatebet', 'Apicontroller@updateBet')->middleware('cors');
+Route::get('/showuser/{id}', 'Apicontroller@showUser')->middleware('cors');
+Route::post('/edituser', 'Apicontroller@editUser')->middleware('cors');
+Route::get('/deleteuser/{id}', 'Apicontroller@deleteUser')->middleware('cors');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
